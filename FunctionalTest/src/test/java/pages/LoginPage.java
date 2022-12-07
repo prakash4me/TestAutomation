d8ded51a46c2dd970e2b25d455cabc5886033e2f
Divya <108125350+Divyaparamsivam@users.noreply.github.com>
2022-12-07 15:03:30 +0530

    Login changes completed

--- FunctionalTest/src/test/java/pages/LoginPage.java
+++ FunctionalTest/src/test/java/pages/LoginPage.java
@@ -11,107 +11,94 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
-	
-	private static WebElement element = null;
-	
-	static WebDriver driver;
-	//Locator for user name field
-	
-	//PageURL
- String BaseURL = "https://pilot.bygglov24.com";
-	
-	
-	By uName = By.xpath("//*[@id=\"Email\"]");
-	//WebElement  uName = driver.findElement(By.id("Email"));
-	
-	//Locator for password field
-	By pswd = By.xpath("//*[@id=\"Password\"]");
-	//WebElement pswd = driver.findElement(By.id("Password"));
-	
-	//Locator for company field 
-	By companyDropdown = By.xpath("//*[@id=\"ddlCompanyList\"]");
-	
-	//Locator for login button
-	By loginBtn = By.xpath("//*[@id=\"signIn\"]");
-	
-	//Locating the admin menu 
-	
-	/*
-	@FindBy(xpath ="//*[@id=\"ddlUser\"]/div[2]/span]")
-	private WebElement adminMenu;
-	*/
-	By adminMenu = By.xpath("//*[@id=\"ddlUser\"]/div[2]/span");
-	
-	//Locator for logout  
-	By logOutMenu = By.xpath("//*[@id=\"ddlUserContent\"]/li[3]/a");
-		
-	//Constructor that will be automatically called as soon as the object of the class is created
-		public LoginPage(WebDriver driver) {
-	          this.driver = driver;
-		}
 
-		
-		private void waitForVisibility(WebElement element) throws Error{
-	           new WebDriverWait(driver, 60)
-	                .until(ExpectedConditions.visibilityOf(element));
-	    }
-		
-		public void BaseURL() {
-			driver.get(BaseURL);
-		}
-		
-		public void CloseBrowser() {
-			driver.close();
-		}
-		public static WebDriver Lanchbrowser() {
-			
-			   System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-				WebDriver driver = new ChromeDriver();
-				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
-				driver.get("https://pilot.bygglov24.com");
-				return driver;
-		   }
-		
-			
-	//Method to enter username
+	private static WebElement element = null;
+
+	static WebDriver driver;
+	// Locator for user name field
+
+	// PageURL
+	String BaseURL = "https://pilot.bygglov24.com";
+
+	// Locator for password field
+	By uName = By.xpath("//*[@id=\"Email\"]");
+
+	// Locator for password field
+	By pswd = By.xpath("//*[@id=\"Password\"]");
+
+	// Locator for company field
+	By companyDropdown = By.xpath("//*[@id=\"ddlCompanyList\"]");
+
+	// Locator for login button
+	By loginBtn = By.xpath("//*[@id=\"signIn\"]");
+
+	// Locator for adminmenu
+	By adminMenu = By.xpath("//*[@id=\"ddlUser\"]/div[2]/span");
+
+	// Locator for logout
+	By logOutMenu = By.xpath("//*[@id=\"ddlUserContent\"]/li[3]/a");
+
+	// Constructor that will be automatically called as soon as the object of the
+	// class is created
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void BaseURL() {
+		driver.get(BaseURL);
+	}
+
+	public static WebDriver Lanchbrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://pilot.bygglov24.com");
+		return driver;
+	}
+
+	// Method to enter username
 	public void enterUsername(String user) {
-		
+
 		driver.findElement(uName).sendKeys(user);
 	}
 
-	//Method to enter password
+	// Method to enter password
 	public void enterPassword(String pass) {
 		driver.findElement(pswd).sendKeys(pass);
 	}
 	/*
-	//Method to select company
-	public void selectCompany(String companyName) {
-		Select company = new Select(companyDropdown);
-		company.selectByVisibleText(companyName);
-	}
-	*/
-	
-	
-	//Method to click on Login button
+	 * //Method to select company public void selectCompany(String companyName) {
+	 * Select company = new Select(companyDropdown);
+	 * company.selectByVisibleText(companyName); }
+	 */
+
+	// Method to click on Login button
 	public void clickLogin() {
 		driver.findElement(loginBtn).click();
 	}
-	
-	public void clickAdminMenu(){
-        // Wait for txtBox to be visible, then send text
-        //waitForVisibility(adminMenu);
-        driver.findElement(adminMenu).click();
+
+	public void clickAdminMenu() {
+		// Wait for txtBox to be visible, then send text
+		// waitForVisibility(adminMenu);
+		driver.findElement(adminMenu).click();
 	}
-	
-	//Method to click logout
+
+	// Method to click logout
 	public void clickLogoutMenu() {
-		//waitForVisibility(logOutMenu);
+		// waitForVisibility(logOutMenu);
 		driver.findElement(logOutMenu).click();
-		
+
 	}
 
+	public void CloseBrowser() {
+		driver.close();
+	}
 
-	
-	
+	/*
+	 * private void waitForVisibility(WebElement element) throws Error{ new
+	 * WebDriverWait(driver, 60) .until(ExpectedConditions.visibilityOf(element)); }
+	 */
+
 }
--- FunctionalTest/src/test/java/testcase/Login.java
+++ FunctionalTest/src/test/java/testcase/Login.java
@@ -9,25 +9,24 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
-
 public class Login {
 
-		WebDriver driver = null;
-	
-	
-	//Login with email 
-    @Test
-	public void loginEmail() throws InterruptedException{
-    driver = LoginPage.Lanchbrowser();
-    LoginPage objLogin = new LoginPage(driver);
-    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-    objLogin.enterUsername("automationtestemail@gmail.com");
-    objLogin.enterPassword("qwerty123");
-   //objLogin.selectCompany("Bygglov24");
-    objLogin.clickLogin();
-	driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-	objLogin.clickAdminMenu();
-	objLogin.clickLogoutMenu();
-	objLogin.CloseBrowser();
-}
+	WebDriver driver = null;
+
+	// Login with email
+	@Test(priority = 1)
+	public void loginEmail() throws InterruptedException {
+		driver = LoginPage.Lanchbrowser();
+		LoginPage objLogin = new LoginPage(driver);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		objLogin.enterUsername("automationtestemail@gmail.com");
+		objLogin.enterPassword("qwerty123");
+		// objLogin.selectCompany("Bygglov24");
+		objLogin.clickLogin();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		objLogin.clickAdminMenu();
+		objLogin.clickLogoutMenu();
+		objLogin.CloseBrowser();
+	}
+
 }
